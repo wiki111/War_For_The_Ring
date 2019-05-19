9c670fae317f43e47d23a0021f5c477306480170
Daniel <maciej.wiqira@gmail.com>
2019-05-19 13:24:19 +0200

    Implemented simple AI attacking algorithm.
    For testing purposes.

--- Assets/Scripts2/AI/AIPlayerScript.cs
+++ Assets/Scripts2/AI/AIPlayerScript.cs
@@ -7,7 +7,9 @@ public class AIPlayerScript : MonoBehaviour
     public GameEvent changeTurnEvent;
     public PlayerVariable currentPlayer;
     public Player aiPlayerData;
+    public Player enemyData;
     public GameSystem gameSystem;
+    public CardSystem cardSystem;
     public BoolVariable isAnimating;
     public GameObject tableView;
 
@@ -26,6 +28,19 @@ public class AIPlayerScript : MonoBehaviour
             {
                 var cardToPlay = aiPlayerData.hand[0];
                 gameSystem.cardSystem.PlaceCardOnTable(cardToPlay.cardView, tableView);
+            }
+
+            if(aiPlayerData.table.Count > 0)
+            {
+                CardInstance cardToPlay = aiPlayerData.table[0];
+                if(enemyData.table.Count > 0)
+                {
+                    cardSystem.UseCard(cardToPlay, enemyData.table[0]);
+                }
+                else
+                {
+                    cardSystem.UseCard(cardToPlay, enemyData);
+                }
             }
 
             if (currentPlayer.value == aiPlayerData)
